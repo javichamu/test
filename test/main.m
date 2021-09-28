ee478870a38ba512d6886c96670ead0c3b6267fb
javier <javier.chamorro@escuelaestech.es>
2021-09-28 20:52:38 +0200

    Initial Commit

---
+++ test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by A1-IMAC17 on 28/9/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
